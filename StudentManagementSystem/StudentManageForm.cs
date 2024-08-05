f4a3df8b0bb1f2b51960316ae2f80341741c05fa
Toan Truong <toantr.work@gmail.com>
2024-08-05 15:59:23 +0700

    Delete unecessary library

--- StudentManagementSystem/StudentManageForm.cs
+++ StudentManagementSystem/StudentManageForm.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.IO;
-using MySql.Data.MySqlClient;
-using Microsoft.VisualBasic.Devices;
+﻿using MySql.Data.MySqlClient;
+
 
 namespace StudentManagementSystem
 {
